79f6cdd85390d783113a0276d5ce66dbd7ba301d
lere dakshak mamman <dshak0672@gmail.com>
2025-04-11 07:56:38 +0000

    initial commit

---
+++ task.js
@@ -0,0 +1,166 @@
+function isPalindrome(str) {
+    let cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let reversed = cleanStr.split('').reverse().join('');
+    return cleanStr === reversed;
+  }
+  
+  console.log(isPalindrome("Racecar"));
+  console.log(isPalindrome("madam"))
+
+
+//   return the largest number in an array of numbers
+
+let arr = [1, 30, 4, 21, 100, 1000];
+let largest = arr[0];
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > largest) {
+        largest = arr[i];
+    }
+}
+console.log(largest);
+
+
+// sum of number in an arr 
+let array = [1, 2, 3, 4, 5];
+let sum = 0;
+for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+}
+console.log(sum);
+
+
+function countVowels(string) {
+    let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    let count = 0;    
+    for (let i = 0; i < string.length; i++) { 
+        for (let j = 0; j < vowels.length; j++) {  
+            if (string[i] === vowels[j]) {  
+                count++; 
+            }
+        }
+    }
+
+    console.log(count);
+}
+countVowels("God"); 
+
+
+let numbers = [4, 10, 2, 99, 23];
+let max = numbers[0];
+function findLargestNumber(arr) {
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+      }
+    }
+    return max;
+  }
+console.log(findLargestNumber(numbers)); 
+
+
+
+
+// create a function that identify a prime number
+
+function isPrime(num) {
+    for (let i = 2; i * i <= num; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+console.log(isPrime(1));
+console.log(isPrime(2)); 
+
+//  in an array of numbers, fine the multiples of any number
+function findMultiples(arr, num) {
+    let multiples = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % num === 0) {
+            multiples.push(arr[i]);
+        }
+    }
+    return multiples;
+}
+console.log(findMultiples([1, 2, 3, 4, 5, 6], 2));  
+
+
+
+// in an array of number fine the index of a number
+function findIndex(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            return i;
+        }
+    }
+    return "nothing found";
+}  
+console.log(findIndex([1, 2, 3, 4, 5], 6)); 
+
+
+
+
+
+let members = [];
+
+function addMember(name, amount) {
+  let member = {
+    name: name,
+    amount: amount
+  };
+  members.push(member);
+  console.log(members);
+}
+
+addMember("praise", 200);
+
+//  Update amount of member
+function updateMember(name, amount) {
+  for (let i = 0; i < members.length; i++) {
+    if (members[i].name === name) {
+      members[i].amount += amount; 
+      break;
+    }
+  }
+  console.log(members);
+}
+updateMember("praise", 100); 
+
+
+// get all members
+function getAllMembers() {
+    console.log("All Members:");
+    for (let i = 0; i < members.length; i++) {
+      console.log(`${members[i].name}: ₦${members[i].amount}`);
+    }
+  }
+getAllMembers();  
+
+// withdraw money from member
+function withdraw(name, amount) {
+  for (let i = 0; i < members.length; i++) {
+    if (members[i].name === name) {
+      if (members[i].amount >= amount) {
+        members[i].amount -= amount;
+        console.log(`${name} withdrew ₦${amount}.Remaining balance: ₦${members[i].amount}`);
+      } else {
+        console.log(`${name} does not have enough balance.`);
+      }
+      break;
+    }
+  }
+}
+withdraw("praise", 100);
+
+// Removed member
+function removeMember(name) {
+  for (let i = 0; i < members.length; i++) {
+    if (members[i].name === name) {
+      members.splice(i, 1);
+      console.log(`${name} has been removed.`);
+      break;
+    }
+  }
+}
+removeMember("praise");
